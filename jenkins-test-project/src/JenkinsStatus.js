34f0b67e858cf0af42bfa3ef044fe94462e41229
TharunnKS <126841693+TharunnKS@users.noreply.github.com>
2025-07-08 19:13:28 +0530

    Update JenkinsStatus.js

--- jenkins-test-project/src/JenkinsStatus.js
+++ jenkins-test-project/src/JenkinsStatus.js
@@ -19,14 +19,14 @@ function JenkinsStatus() {
         flexWrap: 'wrap'
       }}>
         <div style={{margin: '5px'}}>
-          <strong>Build 6</strong>
+          <strong>Build 7</strong>
         </div>
         <div style={{margin: '5px'}}>
           <strong>2025-07-05 19:07:30</strong>
         </div>
       </div>
       <p style={{margin: '15px 0 0 0', fontSize: '14px', opacity: '0.9'}}>
-        Automatically deployed by Jenkins CI/CD Pipeline testing aws 3:30
+        Automatically deployed by Jenkins CI/CD Pipeline testing aws 7:13
       </p>
     </div>
   );
